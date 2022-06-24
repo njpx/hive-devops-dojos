7330b8f37189525e206cab8db85c4dcc2919f1fe
njpx <51145141+njpx@users.noreply.github.com>
2022-06-24 11:56:42 +0700

    Update test.sh

--- testing-strategy-3/scripts/test.sh
+++ testing-strategy-3/scripts/test.sh
@@ -2,5 +2,6 @@
 path=$(pwd)
 e2e_path="${path/scripts/}/e2e-web"
 web_path="${path/scripts/}/web"
-cd $web_path && npm start
+cd $web_path && npm start&
 cd $e2e_path && npm run test
+cd $web_path && npm run stop
